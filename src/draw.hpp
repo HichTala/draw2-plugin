6a226d17177465af7ef1ef9c44624332ebe21561
HichTala <98521878+HichTala@users.noreply.github.com>
2025-06-24 17:59:37 +0200

    Try to resolve C++20 bug

--- src/draw.hpp
+++ src/draw.hpp
@@ -5,6 +5,10 @@
 #ifndef DRAW_FILTER_HPP
 #define DRAW_FILTER_HPP
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <obs-module.h>
 #include <obs-source.h>
 #include <graphics/graphics.h>
@@ -17,11 +21,13 @@ typedef struct draw_filter_data draw_filter_data_t;
 
 struct draw_source_data {
 	obs_source_t *source;
-
 	uint32_t width;
 	uint32_t height;
 };
 typedef struct draw_source_data draw_source_data_t;
+
+extern struct obs_source_info draw_filter;
+extern struct obs_source_info draw_source;
 
 static const char *draw_filter_get_name(void *type_data);
 static const char *draw_source_get_name(void *type_data);
@@ -33,4 +39,8 @@ uint32_t draw_source_get_width(void *data);
 void draw_filter_get_defaults(obs_data_t *settings);
 void draw_source_video_render(void *data, gs_effect_t *effect);
 
-#endif //DRAW_FILTER_HPP
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DRAW_FILTER_HPP
