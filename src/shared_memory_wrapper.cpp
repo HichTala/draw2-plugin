02508c414f76df9771b757d9898e29df0a3f7e37
HichTala <98521878+HichTala@users.noreply.github.com>
2025-07-23 17:03:03 +0200

    Test install boost

--- src/shared_memory_wrapper.cpp
+++ src/shared_memory_wrapper.cpp
@@ -2,8 +2,8 @@
 // Created by HichTala on 23/07/25.
 //
 
-#include <boost/interprocess/shared_memory_object.hpp>
-#include <boost/interprocess/mapped_region.hpp>
+// #include <boost/interprocess/shared_memory_object.hpp>
+// #include <boost/interprocess/mapped_region.hpp>
 
 extern "C" void write_to_shared_memory() {
 
